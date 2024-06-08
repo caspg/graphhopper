c516a3cb1458584ce76e366a645b715e91c3cb01
Kacper Golinski <kacper.golinski@gmail.com>
2024-06-08 09:05:42 +0200

    log output to file

--- run_europe.sh
+++ run_europe.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+file="europe-latest.osm.pbf"
 # wget https://download.geofabrik.de/europe-latest.osm.pbf
 
-java -Xmx32g -Xms32g -Ddw.graphhopper.datareader.file=europe-latest.osm.pbf -jar web/target/graphhopper-web-*.jar server config-example.yml
+java -Xmx32g -Xms32g -Ddw.graphhopper.datareader.file=europe-latest.osm.pbf -jar web/target/graphhopper-web-*.jar server config-example.yml 2>&1 | tee logs.txt
